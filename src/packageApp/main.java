4ec832965edc24d8bede0516511328f0a52e58b0
Ivan Cruz <ivancruhn@gmail.com>
2022-08-19 21:00:05 -0600

    Ejemplo de la aplicacion

---
+++ src/packageApp/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package packageApp;
+
+/**
+ *
+ * @author Developer
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Inicializar un objeto de formulario y mostrar en pantalla
+        pantallaPrincipal loteria = new pantallaPrincipal();
+        loteria.setVisible(true);
+    }
+    
+}
